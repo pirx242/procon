4f8a2a031a5c128777fd2ce25ed1296be26825e3
akol <akol@akol-5810>
2018-01-16 16:32:00 +0100

    BAM

--- procon.py
+++ procon.py
@@ -7,7 +7,7 @@ IPv4_LOCALHOST = '0100007F'
 
 parser = OptionParser()
 parser.add_option('-v', action='store_true', dest='verbose', default=False, help = 'verbose')
-parser.add_option('-p', action='append', type=str, dest='proc_file', default=[], help = 'conf file with process whitelisting (can be repeated)')
+parser.add_option('-p', action='append', type=str, dest='proc_files', default=[], help = 'conf file with process whitelisting (can be repeated)')
 parser.add_option('-a', action='store', type=int, dest='bin_age', default=[], help = 'min age of running binaries (hours)')
 (opts, args) = parser.parse_args(sys.argv[1:])
 
@@ -15,24 +15,18 @@ if os.getuid() != 0:
 	print 'Must run as root!'
 	sys.exit(1)
 
-# read conf from conf files
-proc_whitelist_lines = []
-for f in opts.proc_file:
-	i = 0
-	for line in open(f).readlines():
-		i += 1
-		line = line.strip()
-		if not line or line.startswith('#'):
-			continue
-		proc_whitelist_lines.append(['%s:%i' % (f, i), line])
-
 def alert(severity, alert_string, variables):
-	if severity == 1:
+	if severity == 0:
+		SEV = 'INFORMATIONAL'
+	elif severity == 1:
 		SEV = 'CRITICAL'
 	elif severity == 2:
 		SEV = 'WARNING'
 	else:
 		SEV = 'WHAT'
+
+	if not severity and not opts.verbose:
+		return
 
 	try:
 		print '%s(%s) uid:%s user:%s pid:%s exe:%s comm:%s cmdline:%s runtime:%.2fh exeage:%.2fh' % (SEV, alert_string, variables['uid'], variables['user'], variables['pid'], variables['exe'], variables['comm'], variables['cmdline'], variables['runtime'], variables['exeage'])
@@ -42,30 +36,51 @@ def alert(severity, alert_string, variables):
 		else:
 			print '%s! %s' % (SEV, alert_string)
 
-# parse conf into whitelisting array
-PROC_WHITELIST_LINES_EXPLODED = []
-for where, line in proc_whitelist_lines:
-	i = line.find('#')
-	if i >= 0:
-		line = line[:i]
-	tmp = [col for col in line.split() if col]
-	VARS = tmp[0].lower().split('_')
-	nr_vars = len(VARS)
-	VALS = tmp[1:nr_vars+1]
 
-	if len(VALS) != nr_vars:
-		alert(2, 'Skipping invalid cfg line! %s: %s' % (where, line), {})
-		continue
+def parse_procs_conf():
+	tmp_var_vals = []
 
-	EXTRAS_DICT = {}
-	EXTRAS = tmp[nr_vars+1:]
-	for extra in EXTRAS:
-		var, val = extra.split('=')
-		if var in EXTRAS_DICT.keys():
-			EXTRAS_DICT[var].append(val.split(':'))
-		else:
-			EXTRAS_DICT[var] = [val.split(':')]
-	PROC_WHITELIST_LINES_EXPLODED.append([VARS, VALS, EXTRAS_DICT, where, line, 0])
+	# read conf from conf files
+	proc_whitelist_lines = []
+	for f in opts.proc_files:
+		i = 0
+		for line in open(f).readlines():
+			i += 1
+			line = line.strip()
+			if not line or line.startswith('#'):
+				continue
+			proc_whitelist_lines.append(['%s:%i' % (f, i), line])
+
+	# parse conf into whitelisting array
+	PROC_WHITELIST_LINES_EXPLODED = []
+	for where, line in proc_whitelist_lines:
+		i = line.find('#')
+		if i >= 0:
+			line = line[:i]
+		tmp = [col for col in line.split() if col]
+		VARS = tmp[0].lower().split('_')
+		nr_vars = len(VARS)
+		VALS = tmp[1:nr_vars+1]
+
+		if len(VALS) != nr_vars:
+			alert(1, 'Skipping invalid cfg line! %s: %s' % (where, line), {})
+			continue
+
+		if (VARS, VALS) in tmp_var_vals:
+			raise Exception('line duplicate %s: %s' % (where, line))
+
+		EXTRAS_DICT = {}
+		EXTRAS = tmp[nr_vars+1:]
+		for extra in EXTRAS:
+			var, val = extra.split('=')
+			if var in EXTRAS_DICT.keys():
+				EXTRAS_DICT[var].append(val.split(':'))
+			else:
+				EXTRAS_DICT[var] = [val.split(':')]
+		PROC_WHITELIST_LINES_EXPLODED.append([VARS, VALS, EXTRAS_DICT, where, line, 0])
+		tmp_var_vals.append((VARS, VALS))
+
+	return PROC_WHITELIST_LINES_EXPLODED
 
 def hexip4_to_ip4(hex_ip):
 	r = str(int(hex_ip[6:8], 16))
@@ -74,33 +89,26 @@ def hexip4_to_ip4(hex_ip):
 	r += '.' + str(int(hex_ip[0:2], 16))
 	return r
 
+def get_proc_net_maps():
+	r = {}
 
-PROC_NET_TCP4 = [ line.split() for line in open('/proc/net/tcp').readlines() if line.strip()[:2] != 'sl']
-PROC_NET_TCP4_MAP = {}
-for parts in PROC_NET_TCP4:
-	PROC_NET_TCP4_MAP[parts[9]] = parts
+	PROC_NET_TCP4 = [ line.split() for line in open('/proc/net/tcp').readlines() if line.strip()[:2] != 'sl']
+	PROC_NET_TCP4_MAP = {}
+	# create a map with the inode as key
+	for parts in PROC_NET_TCP4:
+		PROC_NET_TCP4_MAP[parts[9]] = parts
+	r['tcp4'] = PROC_NET_TCP4_MAP
 
-LISTENING_NONLOCALHOST_TCP4_SOCKETS = [ int(parts[1][9:], 16) for parts in PROC_NET_TCP4 if parts[3] == '0A' and parts[1][:8] != IPv4_LOCALHOST]
+	PROC_NET_UDP4 = [ line.split() for line in open('/proc/net/udp').readlines() if line.strip()[:2] != 'sl']
+	PROC_NET_UDP4_MAP = {}
+	# create a map with the inode as key
+	for parts in PROC_NET_UDP4:
+		PROC_NET_UDP4_MAP[parts[9]] = parts
+	r['udp4'] = PROC_NET_UDP4_MAP
 
-PROC_NET_UDP4 = [ line.split() for line in open('/proc/net/udp').readlines() if line.strip()[:2] != 'sl']
-PROC_NET_UDP4_MAP = {}
-for parts in PROC_NET_UDP4:
-	PROC_NET_UDP4_MAP[parts[9]] = parts
+	return r
 
-ALL_LISTENING_TCP4_PORTS = []
-for parts in PROC_NET_TCP4:
-	local = parts[1]
-	state = parts[3]
-	local_port = str(int(local[9:], 16))
-	local_ip4 = local[:8]
 
-	if state != "0A":
-		continue
-
-	if local_ip4 == IPv4_LOCALHOST:
-		continue
-
-	ALL_LISTENING_TCP4_PORTS.append(local_port)
 
 
 NOW_EPOCH = time.time()
@@ -114,68 +122,114 @@ ME = [MY_PID, MY_PPID]
 
 # fetch all running process PIDs, as strings
 PIDS = [pid for pid in os.listdir('/proc') if pid.isdigit() and pid not in ME]
-PIDS = ['997', '1366', '967']
+PIDS = args
 
 def main():
-	nr_procs_invalid = 0
-	nr_procs_checked = 0
-	nr_procs_skipped = 0
+	procs_invalid = []
+	procs_checked = []
+	procs_skipped = []
+	procs_kernel  = []
+
+	try:
+		whitelisting_config = parse_procs_conf()
+	except Exception, e:
+		alert(1, 'Error parsing conf. %s' % (e), None)
+		sys.exit(42)
+
+	proc_net_maps = get_proc_net_maps()
+
+	LISTENING_NONLOCALHOST_TCP4_SOCKETS = [ int(parts[1][9:], 16) for parts in proc_net_maps['tcp4'] if parts[3] == '0A' and parts[1][:8] != IPv4_LOCALHOST]
+	print LISTENING_NONLOCALHOST_TCP4_SOCKETS
+
+	ALL_LISTENING_TCP4_PORTS = []
+	for parts in proc_net_maps['tcp4']:
+		local = parts[1]
+		state = parts[3]
+		local_port = str(int(local[9:], 16))
+		local_ip4 = local[:8]
+
+		if state != "0A":
+			continue
+
+		if local_ip4 == IPv4_LOCALHOST:
+			continue
+
+		ALL_LISTENING_TCP4_PORTS.append(local_port)
+
+	print ALL_LISTENING_TCP4_PORTS
+
 
 	for pid in PIDS:
 		print
-		r = check_process(pid)
-
-		if r == None:
-			nr_procs_skipped += 1
+		print 'checking pid', pid
+		try:
+			v = get_process_variables(pid)
+		except Exception, e:
+			alert(0, e, None)
+			procs_skipped.append(pid)
 			continue
 
-		nr_procs_checked += 1
+		if proc_is_kernel(v):
+			print 'pid is kernel', pid
+			procs_kernel.append(pid)
+			continue
 
-		if r == True:
-			pass
+		procs_checked.append(pid)
+
+		if not check_process_ok(pid, whitelisting_config, proc_net_maps):
+			procs_invalid.append(pid)
 		else:
-			nr_procs_invalid += 1
+			print pid, 'ok'
 
-	print 'checked', nr_procs_checked
-	print 'skipped', nr_procs_skipped
-	print 'invalid', nr_procs_invalid
+	print
+	print 'checked', procs_checked
+	print 'skipped', procs_skipped
+	print 'invalid', procs_invalid
+	print 'kernel', procs_kernel
 
-	if nr_procs_invalid:
+	if procs_invalid:
 		sys.exit(2)
 	else:
 		sys.exit(0)
 
 
-def check_process(pid):
-	print 'checking pid', pid
-
-	v = get_process_variables(pid)
-	print v
-
+def check_process_ok(pid, whitelisting_config, proc_net_maps):
 	try:
+		v = get_process_variables(pid)
 		ofs = get_process_open_files(pid)
-		print ofs
-	except OSError, e:
-		alert(2, 'could not get list of open files', locals())
-		return None
+	except Exception, e:
+		alert(0, e, None)
+		return True
 
-	check_process_variables_files(v, ofs)
-
-	return True
-
-
-def check_process_variables_files(variables, open_files):
-	pass
+	return check_process_variables_files(pid, v, ofs, whitelisting_config, proc_net_maps)
 
 
 def get_process_variables(pid):
 	pid_dir = os.path.join('/proc', pid) + os.sep
+	exe = None
 
-	# procs UID
+	uid = str(os.stat(pid_dir).st_uid)
+
+	# procs exe file
+	exeage = 0.0
 	try:
-		uid = str(os.stat(pid_dir).st_uid)
-	except OSError, e:
-		return {}
+		exe = os.readlink(pid_dir + 'exe')
+		if ' (deleted)' in exe:
+			exeage = (NOW_EPOCH - os.stat(exe[:-10]).st_mtime) / 3600.0
+			alert(1, 'deleted file running', proc_variables())
+			exe = exe[:-10]
+		else:
+			# executable files age (since last modified)
+			exeage = (NOW_EPOCH - os.stat(exe).st_mtime) / 3600.0
+	except OSError:
+		exe = None
+		exeage = 0
+
+		if uid == '0':
+			#kernel process
+			pass
+		else:
+			alert(1, 'proc with no exe and not running as root', locals())
 
 	# procs user name
 	try:
@@ -209,40 +263,20 @@ def get_process_variables(pid):
 	# procs command name
 	comm = open(pid_dir + 'comm').read().strip()
 
-	# procs exe file
-	exeage = 0.0
-	try:
-		exe = os.readlink(pid_dir + 'exe')
-		if ' (deleted)' in exe:
-			exeage = (NOW_EPOCH - os.stat(exe[:-10]).st_mtime) / 3600.0
-			alert(1, 'deleted file running', locals())
-			exe = exe[:-10]
-		else:
-			# executable files age (since last modified)
-			exeage = (NOW_EPOCH - os.stat(exe).st_mtime) / 3600.0
-	except OSError:
-		exe = None
-		exeage = 0
-
-		if uid == '0':
-			#kernel process
-			return locals()
-		else:
-			alert(1, 'proc with no exe and not running as root', locals())
-			return locals()
-
-	result = True
 	return locals()
 
+def proc_is_kernel(v):
+	return v['exe'] is None
+
 def get_process_open_files(pid):
-	'''Returns a list of open files. Skips stuff like pipes, /dev/null and such.
+	'''Returns a dict of open files. Skips stuff like pipes, /dev/null and such.
 
 	Args:
 		pid (str):	the PID
 
 	Returns:
-		List of lists. Like e.g.
-		[['regular', '/foo/bar'], ['tcp4', '0A', '00000000', '22', '00000000', '0']]
+		Dict of lists. Like e.g.
+		...
 
 		An IP socket contains [protocol, state, local_ip4, local_port, remote_ip4, remote_port]
 		A regular file contains ['regular', filename]
@@ -303,7 +337,7 @@ def get_ip4_socket_from_inode(inode, proc_net_map):
 		# 						break
 
 		# 			if not ok:
-		# 				alert(1, 'process %s listens on non-localhost port %s' % (comm, local_port), LOCALS)
+		# 				alert(1, 'process %s listens on non-localhost port %s' % (comm, local_port), proc_variables)
 
 		# 		else:
 		# 			if remote_ip4 == IPv4_LOCALHOST:
@@ -313,7 +347,7 @@ def get_ip4_socket_from_inode(inode, proc_net_map):
 		# 				if 'NET_CON_IN' in EXTRAS_DICT:
 		# 					pass
 		# 				else:
-		# 					alert(1, 'process %s has an inbound connection to local %s:%s from remote %s:%s in state %s' % (comm, hexip4_to_ip4(local_ip4), local_port, hexip4_to_ip4(remote_ip4), remote_port, state), LOCALS)
+		# 					alert(1, 'process %s has an inbound connection to local %s:%s from remote %s:%s in state %s' % (comm, hexip4_to_ip4(local_ip4), local_port, hexip4_to_ip4(remote_ip4), remote_port, state), proc_variables)
 		# 			else:
 		# 				if 'NET_CON_OUT' in EXTRAS_DICT:
 		# 					for ip, port in EXTRAS_DICT['NET_CON_OUT']:
@@ -323,95 +357,93 @@ def get_ip4_socket_from_inode(inode, proc_net_map):
 
 		# 				if not ok:
 		# 					if state == '01':
-		# 						alert(1, 'process %s has an active outbound connection to remote %s:%s' % (comm, hexip4_to_ip4(remote_ip4), remote_port), LOCALS)
+		# 						alert(1, 'process %s has an active outbound connection to remote %s:%s' % (comm, hexip4_to_ip4(remote_ip4), remote_port), proc_variables)
 		# 					else:
-		# 						alert(1, 'process %s has/had an outbound connection to remote %s:%s, now in state %s' % (comm, hexip4_to_ip4(remote_ip4), remote_port, state), LOCALS)
+		# 						alert(1, 'process %s has/had an outbound connection to remote %s:%s, now in state %s' % (comm, hexip4_to_ip4(remote_ip4), remote_port, state), proc_variables)
 
 		# elif of.startswith('/etc'):
-		# 	alert(2, 'process has open file in /etc, of=%s' % (of), LOCALS)
+		# 	alert(2, 'process has open file in /etc, of=%s' % (of), proc_variables)
 		# else:
 		# 	pass
 
-
-def check_process_old(pid):
-
+def check_process_variables_files(pid, proc_variables, open_files, whitelisting_config, proc_net_maps):
 	proc_is_whitelisted = False
-	for whitelist in PROC_WHITELIST_LINES_EXPLODED:
-		try:
-			VARS = whitelist[0]
-			VALS = whitelist[1]
-			EXTRAS_DICT = whitelist[2]
-			WHERE = whitelist[3]
-			LINE = whitelist[4]
-			HITS = whitelist[5]
 
-			LOCALS = locals()
+	for line_list in whitelisting_config:
+		VARS, VALS, EXTRAS_DICT, where, line, hits = line_list
 
-			proc_is_whitelisted = True
-			for i in range(len(VARS)):
-				vr = VARS[i]
-				vl = VALS[i]
-				
-				if vr in ['exe', 'cmdline'] and vl.endswith('>'):
-					vl = vl[:-1]
-					if not LOCALS[vr].startswith(vl):
+		proc_is_whitelisted = True
+		for vr, vl in zip(VARS, VALS):
+			
+			if vr in ['exe', 'cmdline'] and vl.endswith('>'):
+				vl = vl[:-1]
+				if not proc_variables[vr].startswith(vl):
+					proc_is_whitelisted = False
+					break
+			elif vr == 'pid':
+				if vl[0] == '<':
+					if int(pid) < int(vl[1:]):
+						pass
+					else:
 						proc_is_whitelisted = False
 						break
-				elif vr == 'pid':
-					if vl[0] == '<':
-						if int(pid) < int(vl[1:]):
-							pass
-						else:
-							proc_is_whitelisted = False
-							break
-					elif vl[0] == '>':
-						if int(pid) > int(vl[1:]):
-							pass
-						else:
-							proc_is_whitelisted = False
-							break
+				elif vl[0] == '>':
+					if int(pid) > int(vl[1:]):
+						pass
 					else:
-						if int(vl) != int(pid):
-							proc_is_whitelisted = False
-							break
-				elif vr == 'uid':
-					if vl[0] == '<':
-						if int(uid) < int(vl[1:]):
-							pass
-						else:
-							proc_is_whitelisted = False
-							break
-					elif vl[0] == '>':
-						if int(uid) > int(vl[1:]):
-							pass
-						else:
-							proc_is_whitelisted = False
-							break
-					else:
-						if int(vl) != int(uid):
-							proc_is_whitelisted = False
-							break
+						proc_is_whitelisted = False
+						break
 				else:
-					if LOCALS[vr] != vl:
+					if int(vl) != int(pid):
 						proc_is_whitelisted = False
 						break
+			elif vr == 'uid':
+				if vl[0] == '<':
+					if int(uid) < int(vl[1:]):
+						pass
+					else:
+						proc_is_whitelisted = False
+						break
+				elif vl[0] == '>':
+					if int(uid) > int(vl[1:]):
+						pass
+					else:
+						proc_is_whitelisted = False
+						break
+				else:
+					if int(vl) != int(uid):
+						proc_is_whitelisted = False
+						break
+			else:
+				if proc_variables[vr] != vl:
+					proc_is_whitelisted = False
+					break
 
 			vr = 'OPEN FILES'
 			vl = None
 
 
-			if proc_is_whitelisted:
-				break
-		except IOError:
-			pass
-		#except Exception, e:
-			#errstr = 'Problem with %s=%s in whitelisting line %s' %(vr, vl, whitelist)
-			#raise Exception(errstr)
+		if proc_is_whitelisted:
+			# it got whitelisted by the last cfg line, the variables at least
+			break
 
+	if proc_is_whitelisted:
+		pass
+
+
+	return proc_is_whitelisted
+
+
+
+
+
+def check_process_old(pid):
+
+	
 	if proc_is_whitelisted:
 		return True
 	else:
-		alert(1, 'bad process', LOCALS)
+		alert(1, 'bad process', proc_variables)
 		return False
 
 
